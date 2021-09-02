f1d4deaa5ff225c6d79d94074ba56835fd5a1bb3
acnetofrc <acneto.frc@hotmail.com>
2021-09-02 01:26:55 +0000

    2º menu

--- menu/src/componentes/Missao/Missao.jsx
+++ menu/src/componentes/Missao/Missao.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import './Parceiros.css'
+import './Missao.css'
 import Titulo from '../Titulo/Titulo.jsx'
 
 export default function Missao() {
     return (
         <div>
 
-            <Titulo texto = "Seção de Parceiros" />
+            <Titulo texto = "Seção de Missão" />
 
-            <div className="parceiros">
+            <div className="missao">
 
             </div>
 
---
+++ menu/src/componentes/Sobre/Sobre.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Sobre.css'
+import Titulo from '../Titulo/Titulo.jsx'
+
+export default function Sobre() {
+    return (
+        <div>
+
+            <Titulo texto = "Seção de Sobre" />
+
+            <div className="sobre">
+
+            </div>
+
+        </div>
+
+    )
+}
+
